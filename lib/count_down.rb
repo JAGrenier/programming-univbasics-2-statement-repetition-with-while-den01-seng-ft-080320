9eab0cdb9812960941d35718181e24ae5ec7a80b
Jennifer Grenier <jenniferagrenier@gmail.com>
2020-05-27 23:56:24 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,7 +1,5 @@
-magic_exit_number = 7
-count = 0
-while count < 10 do
-  break if count == magic_exit_number
-  puts "I am the #{count}, I love to count!" # Work
-  count = count + 1
+count = 11 
+while count < 3 do 
+  puts count 
+  count = count -1 
 end
